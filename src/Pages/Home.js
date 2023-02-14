4b928430ca5bed6de8e916b3b5db655ffa8a3d5e
dsaf2007 <dsaf2007@naver.com>
2023-02-14 18:19:00 +0900

    feat : add function to lefChild and rightChild. leftChild : decrease Month when button clicked rightChild : increase Month when button clicked

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,12 +9,20 @@ const Home = () => {
 
     const headDate = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월`;
 
+    const increaseMonth = () => {
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1,curDate.getDate()));
+    }
+
+    const decreaseMonth = () => {
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate()));
+    }
+
     return(
     <div>
         <MyHeader
         headText={headDate}
-        leftChild={<MyButton text={"<"} onClick={() =>{}}/>}
-        rightChild={<MyButton text={">"} onClick={()=>{}}/>}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth}/>}
+        rightChild={<MyButton text={">"} onClick={increaseMonth}/>}
         />
         <h1>Home</h1>
         <p>이곳은 홈 입니다.</p>
