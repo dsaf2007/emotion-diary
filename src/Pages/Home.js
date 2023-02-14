4fdb76a2123c3b605e856886bab217a1e1803020
dsaf2007 <dsaf2007@naver.com>
2023-02-14 18:14:30 +0900

    feat : add Header to Home page. only design, no fuction added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,20 @@
+import {useState} from "react";
+
+import MyHeader from "./../Components/MyHeader";
+import MyButton from "./../Components/MyButton"
+
 const Home = () => {
+
+    const [curDate, setCurDate] = useState(new Date());
+
+    const headDate = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월`;
+
     return(
     <div>
+        <MyHeader
+        headText={headDate}
+        leftChild={<MyButton text={"<"} onClick={() =>{}}/>}
+        rightChild={<MyButton text={">"} onClick={()=>{}}/>}
         <h1>Home</h1>
         <p>이곳은 홈 입니다.</p>
     </div>);
