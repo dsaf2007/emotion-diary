b1fa39179c5c9c997ab7c2a95c46b550cf18c398
dsaf2007 <dsaf2007@naver.com>
2023-02-20 01:17:39 +0900

    feat : add date input in New page. add header to page

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -1,9 +1,32 @@
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+import MyHeader from "./../Components/MyHeader";
+import MyButton from "./../Components/MyButton";
 const New = () => {
+
+    const [date,setData] = useState();
+
+    const navigate = useNavigate();
     return(
     <div>
-        <h1>New</h1>
-        <p>이곳은 new입니다</p>
-    </div>)
+        <MyHeader
+        headText="{새 일기 쓰기}"
+        leftchild={<MyButton text={"< 뒤로가기"}/>}
+        />
+        <div>
+        <section>
+            <h4>오늘은 언제인가요?</h4>
+            <div className="input-box">
+                <input 
+                value={date}
+                onChange={(e)=>setImmediate(e.target.value)}
+                type="date"/>
+            </div>
+        </section>
+    </div>
+    </div>
+    )
 }
 
 export default New;
