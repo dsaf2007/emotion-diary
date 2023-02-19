431d9aaeb0d3729c1ba5b112edc5bd1cb4a79a9f
dsaf2007 <dsaf2007@naver.com>
2023-02-20 01:59:16 +0900

    feat : add new component : EmotionItem.js. gets emotionlist as key and shows emotion

--- src/Components/DiaryEditor.js
+++ src/Components/DiaryEditor.js
@@ -3,6 +3,35 @@ import {useState} from "react";
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
+import EmotionItem from "./EmotionItem"; 
+
+const emotionList = [
+    {
+        emotion_id:1,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotipon1.png`,
+        emotion_descript:'완전 좋음'
+    },
+    {
+        emotion_id:2,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotipon2.png`,
+        emotion_descript:'좋음'
+    },
+    {
+        emotion_id:3,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotipon3.png`,
+        emotion_descript:'보통'
+    },
+    {
+        emotion_id:4,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotipon4.png`,
+        emotion_descript:'나쁨'
+    },
+    {
+        emotion_id:5,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotipon5.png`,
+        emotion_descript:'완전 나쁨'
+    },
+]
 
 const getStringDate = (date) => {
     return date.toISOString().slice(0,10);
@@ -29,7 +58,17 @@ const DiaryEditor = () => {
                 type="date"/>
             </div>
         </section>
+        <section>
+            <h4>오늘의 감정</h4>
+            <div className="input_box emotion_list_wrapper">
+                {emotionList.map((it)=>(
+                    <EmotionItem key={it.emotion_id} {...it}/>
+                    ))}
+            </div>
+        </section>
     </div>
     </div>
     )
 }
+
+export default DiaryEditor;
