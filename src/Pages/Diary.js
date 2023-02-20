ea97dc61a749fb71f449bf0f96a336f846142038
dsaf2007 <dsaf2007@naver.com>
2023-02-21 00:55:53 +0900

    feat : add emotion component in Diary page

--- src/Pages/Diary.js
+++ src/Pages/Diary.js
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DiaryStateContext } from "../App";
 import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
 import MyHeader from "../Components/MyHeader";
 import MyButton from "../Components/MyButton";
@@ -29,16 +30,37 @@ const Diary = () => {
 	if (!data) {
 		return <div className="DiaryPage">로딩중입니다.</div>;
 	} else {
+		const curEmotionData = emotionList.find(
+			(it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+		);
 		return (
 			<div className="DiaryPage">
-				<MyHeader headText={`${getStringDate(new Date(data.date))} 기록`}
-                leftChild={
-                    <MyButton text={"< 뒤로가기"} onClick={()=> navigate(-1)}/>
-                }
-                rightChild={
-                    <MyButton text={"수정하기"} onClick={() => navigate(`/edit/${data.id}`)}/>
-                }
-                />
+				<MyHeader
+					headText={`${getStringDate(new Date(data.date))} 기록`}
+					leftChild={
+						<MyButton
+							text={"< 뒤로가기"}
+							onClick={() => navigate(-1)}
+						/>
+					}
+					rightChild={
+						<MyButton
+							text={"수정하기"}
+							onClick={() => navigate(`/edit/${data.id}`)}
+						/>
+					}
+				/>
+                <article>
+                    <section>
+                        <h4>오늘의 감정</h4>
+                        <div className="diary_img_wrapper">
+                            <img src={curEmotionData.emotion_img} />
+                            <div className="emotion_descript">
+                                {curEmotionData.emotion_descript}
+                            </div>
+                        </div>
+                    </section>
+                </article>
 			</div>
 		);
 	}
