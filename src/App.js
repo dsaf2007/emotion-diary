41578aaef880dd89bbf64c944da248c4d710df2e
dsaf2007 <dsaf2007@naver.com>
2023-02-23 15:28:51 +0900

    feat : init data using localStorage

--- src/App.js
+++ src/App.js
@@ -82,12 +82,19 @@ function App() {
 
 
     useEffect(()=> {
-        localStorage.setItem('key',10);
+        const localData = localStorage.getItem('diary');
+        if(localData)
+        {
+            const diaryList = JSON.parse(localData).sort((a,b) => parseInt(b.id) - parseInt(a.id));
+            dataId.current = parseInt(diaryList[0].id) + 1;
+
+            dispatch({type:"INIT", data:diaryList});
+        }
     },[] )
 
     const [data, dispatch] = useReducer(reducer, []);
 
-    const dataId = useRef(6);
+    const dataId = useRef(0);
 
     console.log(new Date().getTime())
 
